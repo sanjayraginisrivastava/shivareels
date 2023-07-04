f70381c61984669d63289237c6993584e19342de
sanjayraginisrivastava <134097155+sanjayraginisrivastava@users.noreply.github.com>
2023-07-04 12:50:15 +0530

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <ul className='nav-menu'>
+            <li>SignUp</li>
+            <li>SignIn</li>
+            <li>Profile</li>
+        </ul>
+    </div>
+  )
+}
